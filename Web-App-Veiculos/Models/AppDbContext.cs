837095e0000a596469678d9efc834c675b104167
Vagner de Oliveira Florencio <113205762+v4gn32@users.noreply.github.com>
2023-10-27 17:01:22 -0300

    Configuração do banco de dados e da tabela veiculo

---
+++ Web-App-Veiculos/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Web_App_Veiculos.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
--- Web-App-Veiculos/Program.cs
+++ Web-App-Veiculos/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Web_App_Veiculos.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
