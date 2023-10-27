26eea7e1d672357a42a32154f7d43df81e7bcab9
Vagner de Oliveira Florencio <113205762+v4gn32@users.noreply.github.com>
2023-10-27 17:28:38 -0300

    Configuração do Create

---
+++ Web-App-Veiculos/Controllers/VeiculosController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_App_Veiculos.Models;
+
+namespace Web_App_Veiculos.Controllers
+{
+    public class VeiculosController : Controller
+    {
+        private readonly AppDbContext _context;
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Veiculos.ToArrayAsync();
+            
+            return View(dados);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Veiculo veiculo)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Veiculos.Add(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            
+            return View(veiculo);
+        }
+    }
+}
