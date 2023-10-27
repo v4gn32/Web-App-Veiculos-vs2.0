91fccd82cf2fd84a24bd190b28ce94058d29eb93
Vagner de Oliveira Florencio <113205762+v4gn32@users.noreply.github.com>
2023-10-27 16:36:50 -0300

    Adicionar arquivos de projeto.

---
+++ Web-App-Veiculos/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web_App_Veiculos.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Obrigatório Informar o Nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage ="Obrigatório informar a Placa")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Ano de Fabricação")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Ano do Modelo")]
+        public int AnoModelo { get; set; }
+    }
+}
